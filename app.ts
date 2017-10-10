97a387098861a7caeda05c12068b4289be6b2d42
musfiqurcse <md.musfiqurcse@outlook.com>
2017-10-10 18:46:27 +0600

    Funciton module added

--- app.ts
+++ app.ts
@@ -5,3 +5,34 @@ class Test{
 }
 var hello = new Test('Testing')
 console.log('testing')
+function GetAllBooks(): Array<Object>{
+    let books = [
+        { title: 'Te45',  author: 'James Joyece', available: true},
+        { title: 'Test02',  author: 'Thomas Kelvin', available: false},
+        { title: 'Test03',  author: 'Tiger Wood', available: true}
+    ];
+    return books;
+}
+function LogFirstAvailable(books){
+    let numberofBooks = books.length;
+    let firstAvailable = '';
+    for(let currentbook of books){
+        if(currentbook.available){
+            firstAvailable = currentbook.title;
+            break;
+        }
+    }
+    console.log('Total Books: '+numberofBooks);
+    console.log('First Available: '+ firstAvailable);
+
+}
+const allBooks = GetAllBooks();
+allBooks.push({ title: 'Test04',  author: 'Signi Sood', available: true});
+LogFirstAvailable(allBooks);
+
+enum Category {Proced, Fail, Success};
+
+let setValue: Category = Category.Success;
+console.log(Category[setValue]);
+//Array Usages
+let stringArray: Array<string> = ['top','bottom','corner'];
